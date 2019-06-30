5422b4119ea3f6151c1f02fb4b1bf74aa884094c
Vladislav <vlad119955@gmail.com>
2019-06-30 21:59:10 +0300

    Delete Tranc2.java

--- src/Tranc2.java
+++
@@ -1,9 +0,0 @@
-class Trunc2 {
-    Httpreq req = new Httpreq();
-    Runnable runs = () -> {
-        try {
-            req.httppost("Второй транзакционный реестр пришел?");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    };}
